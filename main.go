86260302c38fe8a7b249924cd1e41990c14f3165
Артем Чаадаев <perfectgentleman.de@ya.ru>
2022-05-14 13:16:26 +0300

    better code I think

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"errors"
 	"flag"
+	"fmt"
 	"log"
 	"os"
 	"strings"
@@ -9,32 +11,39 @@ import (
 	"github.com/skip2/go-qrcode"
 )
 
+func readPayload(message, inputFilename string) (string, error) {
+	if message != "" {
+		return message, nil
+	}
+
+	if inputFilename == "" {
+		return "", errors.New("no any input selected, check '-h' flag")
+	}
+
+	if splitParts := strings.Split(inputFilename, "."); len(splitParts) == 0 || splitParts[len(splitParts)-1] != "txt" {
+		return "", errors.New("wrong file extension, check '-h' flag")
+	}
+
+	bytes, err := os.ReadFile(inputFilename)
+	if err != nil {
+		return "", fmt.Errorf("cannot read file %s: %w", inputFilename, err)
+	}
+
+	return string(bytes), nil
+}
+
 func main() {
 	message := flag.String("m", "", "message input (in priority if other inputs chosen)")
 	outputFilename := flag.String("o", "unknown.png", "output filename")
 	inputFilename := flag.String("i", "", "input filename (.txt only)")
 	flag.Parse()
 
-	payload := ""
-
-	switch {
-	case *message != "":
-		payload = *message
-	case *inputFilename != "":
-		if splitParts := strings.Split(*inputFilename, "."); len(splitParts) == 0 || splitParts[len(splitParts)-1] != "txt" {
-			log.Fatal("wrong file extension")
-		}
-
-		bytes, err := os.ReadFile(*inputFilename)
-		if err != nil {
-			log.Fatalf("cannot read file %s: %v", *inputFilename, err)
-		}
-		payload = string(bytes)
-	default:
-		log.Fatal("no any input selected, check '-h' flag")
+	payload, err := readPayload(*message, *inputFilename)
+	if err != nil {
+		log.Fatalf("cannot read input: %v", err)
 	}
 
-	err := qrcode.WriteFile(payload, qrcode.Medium, 256, *outputFilename)
+	err = qrcode.WriteFile(payload, qrcode.Medium, 256, *outputFilename)
 	if err != nil {
 		log.Fatalf("cannot write file: %v", err)
 	}
