375a639666533cd206983ce368596f31f894f306
Артем Чаадаев <perfectgentleman.de@ya.ru>
2022-05-14 03:08:12 +0300

    it works, but I don't like the code

--- main.go
+++ main.go
@@ -2,24 +2,39 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	"log"
+	"os"
+	"strings"
 
 	"github.com/skip2/go-qrcode"
 )
 
 func main() {
-	message := flag.String("m", "no message", "message input")
+	message := flag.String("m", "", "message input (in priority if other inputs chosen)")
 	outputFilename := flag.String("o", "unknown.png", "output filename")
-	inputFilename := flag.String("i", "input.txt", "input filename")
-
+	inputFilename := flag.String("i", "", "input filename (.txt only)")
 	flag.Parse()
 
-	fmt.Println(*message)
-	fmt.Println(*inputFilename)
-	fmt.Println(*outputFilename)
+	payload := ""
 
-	err := qrcode.WriteFile(*message, qrcode.Medium, 256, *inputFilename)
+	switch {
+	case *message != "":
+		payload = *message
+	case *inputFilename != "":
+		if splitParts := strings.Split(*inputFilename, "."); len(splitParts) == 0 || splitParts[len(splitParts)-1] != "txt" {
+			log.Fatal("wrong file extension")
+		}
+
+		bytes, err := os.ReadFile(*inputFilename)
+		if err != nil {
+			log.Fatalf("cannot read file %s: %v", *inputFilename, err)
+		}
+		payload = string(bytes)
+	default:
+		log.Fatal("no any input selected, check '-h' flag")
+	}
+
+	err := qrcode.WriteFile(payload, qrcode.Medium, 256, *outputFilename)
 	if err != nil {
 		log.Fatalf("cannot write file: %v", err)
 	}
